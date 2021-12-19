412d7a0d715dfb2abf658a55d7fabde23f1baf75
Saulius Krasuckas <saulius2@gmail.com>
2021-12-19 17:39:51 +0200

    Add files via upload

--- directdraw-1/some.c
+++ directdraw-1/some.c
@@ -84,9 +84,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	}
 	printf("OK IDirectDraw_SetCooperativeLevel()\n");
 
-	rc = IDirectDraw_SetDisplayMode(dd_obj, 640, 480, 32);
+	rc = IDirectDraw_SetDisplayMode(dd_obj, 640, 480, 8);
 	if (rc != DD_OK) {
-		printf("NO IDirectDraw_SetDisplayMode(): GLE = %ld\n", GetLastError());
 		IDirectDraw_Release(dd_obj);
 		return 1;
 	}
